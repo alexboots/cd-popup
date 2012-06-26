7fa420b25c9fd2325339e87532e20633cebe3710
Alex Hays <alexhays@gmail.com>
2012-06-26 16:22:21 -0500

    removed three-row high title ala dylan n cheney's opinion

--- js/layout-change.js
+++ js/layout-change.js
@@ -2,13 +2,15 @@
 	    $('.textfillCode').textfill({ maxFontPixels: 70 });	
 		
 		var codeLen = jQuery('.couponInfo-layout').text().length;
-		if((codeLen > 60) && (codeLen < 100)) //changes layout based on how long the title is
+	
+		if(codeLen > 60) //&& (codeLen < 100)) //changes layout based on how long the title is
 		{
 			$('.pop-title-info').css('height', 42);
 			$('.pop-title-info').css('margin-top', 0);
 			$('.pop-couponInfo').css('line-height', '21px');
 			$('.pop-storeName').css('line-height', '41px');						
 		}
+		/* This is for a three-row long title. Uncomment this, and the '//&& (codeLen' bit above and it will be live again. [Make sure to add a '(' after the 'if'.]
 		if(codeLen > 100)
 		{
 			$('.pop-title-info').css('height', 62);
@@ -17,5 +19,5 @@
 			$('.pop-storeName').css('line-height', '65px');	
 			$('.pop-storeName').css('line-height', '65px');	
 			$('.pop-couponInfo').css('font-size', 13);								
-		}																											    
+		}	*/																									    
 	});
